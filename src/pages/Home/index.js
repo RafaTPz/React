c39ea5f1febff9bf28b948e305442f654d84c562
unknown <35443@fiecalunos.lan>
2024-03-19 20:26:52 -0300

    Aprendendo o hook useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ContainerPage, TitlePage } from "../../components/Main";
 import { AreaHome } from "./styled";
 
 const Page = () => {
+
+    const [contador, setContador] = useState(0);
+
     return (
         <ContainerPage>
             <AreaHome>
             <TitlePage>Welcome to the Dylanx, the biggest White Tiger</TitlePage>
             <div className="tigerimg"><img src="/tigrao.png" /></div>
+            <br />
+            <br />
+            <button onClick={
+                () => setContador(contador + 1)}>aumenta ai</button>
+
+                <p>{contador}</p>
+            
             </AreaHome>
         </ContainerPage>
         
